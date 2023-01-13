f3f8876fefff82cd8a88df4318ce42775caf937d
denil <deniljohnms27@gmail.com>
2023-01-13 19:24:16 +0530

    four

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,15 +2,20 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args) {
         B b = new B();
-        //System.out.println(b.meth());
+        //System.out.println();
+        b.meth();
+    }
+
+
+
 }
 
 class A{
     String meth(){
         return "Invoking method from class A";
-
-    }
+   }
 }
 class B extends A{
     String meth(){
